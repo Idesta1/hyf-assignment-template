5cc45e1f8af002f029679941c58d5ac1344f5c86
Iglesia Desta <iglesiad25@gmail.com>
2025-11-06 09:58:04 +0100

    Create pizza.js
    exercise

---
+++ courses/foundation/intro-to-javascript/excercise/pizza.js
@@ -0,0 +1,20 @@
+let favoritePizza = "margarita";
+let margaritaPrice = 100;
+let pizzaCount = 5;
+let takeAway = true;
+
+const totalPrice = margaritaPrice * pizzaCount;
+
+console.log(
+  "New pizza order:" +
+    " Is it takeaway?:" +
+    takeAway +
+    " " +
+    pizzaCount +
+    " " +
+    favoritePizza +
+    "." +
+    " Total cost for your order is " +
+    totalPrice +
+    " Kroner."
+);
