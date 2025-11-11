626e0eea4872ae8c7108f66f7eac87e6a4b98baa
Iglesia Desta <iglesiad25@gmail.com>
2025-11-11 13:05:41 +0100

    Create flight.js
    did task 1

---
+++ courses/foundation/intro-to-javascript/week2/flight.js
@@ -0,0 +1,14 @@
+//Flight booking fullname function
+function getFullName(firstName, surName) {
+  const fullName = firstName + " " + surName;
+  return fullName;
+}
+
+const fullName1 = "Sam";
+const fullName2 = "David";
+
+console.log(getFullName("Benjamin", "Hughes"));
+
+console.log(getFullName(fullName1, fullName2));
+
+//Formal fullname
