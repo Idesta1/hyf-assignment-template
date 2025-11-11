f86ff94e11210462e8f1d46e501e1599a32a0487
Iglesia Desta <iglesiad25@gmail.com>
2025-11-11 14:27:03 +0100

    Update flight.js
    added conditions and two more parameters.

--- courses/foundation/intro-to-javascript/week2/flight.js
+++ courses/foundation/intro-to-javascript/week2/flight.js
@@ -1,14 +1,28 @@
 //Flight booking fullname function
-function getFullName(firstName, surName) {
+function getFullName(firstName, surName, useFormalName, gender) {
+  const formalName1 = "Lord";
+  const formalName2 = "Madam";
   const fullName = firstName + " " + surName;
-  return fullName;
+  if (useFormalName === true && gender === "man") {
+    return formalName1 + " " + fullName;
+  } else if (useFormalName === true && gender === "woman") {
+    return formalName2 + " " + fullName;
+  } else {
+    return fullName;
+  }
 }
 
 const fullName1 = "Sam";
 const fullName2 = "David";
 
-console.log(getFullName("Benjamin", "Hughes"));
+console.log(getFullName("Benjamin", "Hughes", true, "man"));
+console.log(getFullName("Sarah", "Johnson", false, "woman"));
+console.log(getFullName("Dina", "Smith", true, "woman"));
 
 console.log(getFullName(fullName1, fullName2));
 
 //Formal fullname
+//to add formal name i  added useFormalName another parameter and used if else condition and added boolean value to check if true to add formal name if false then just fullname.
+// i declared two variables formalName1 and formalName2 to store "Lord" and "Madam".
+//i called the function twice with different values to check if it works correctly.
+// to add gender options i added another parameter gender and used if else condition with "man" and "woman".
