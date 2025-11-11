0eeffa6fffa063eaf0706afd1a0027f535fd9bbc
Iglesia Desta <iglesiad25@gmail.com>
2025-11-11 21:51:07 +0100

    Create weatherwear.js

---
+++ courses/foundation/intro-to-javascript/week2/weatherwear.js
@@ -0,0 +1,19 @@
+function whatToWear(temperature) {
+  const winter = `Jacket , pants  and  sweater`;
+  const summer = `t-shirt , shorts and summer-dress`; //using template literals
+
+  if (temperature >= 17) {
+    return "You can wear" + " " + summer;
+  } else if (temperature < 17) {
+    return "You can wear" + " " + winter;
+  } else {
+    return "You can always decide what to wear";
+  }
+}
+
+console.log(whatToWear(20));
+console.log(whatToWear(10));
+console.log(whatToWear());
+
+//added temperature parameter and used if else condition to return whatToWear,
+//used template literal to concatenating strings
