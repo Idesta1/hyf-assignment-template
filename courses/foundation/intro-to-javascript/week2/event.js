0702e39f82e1004e0cb217b0e314996bd071a8eb
Iglesia Desta <iglesiad25@gmail.com>
2025-11-12 19:28:57 +0100

    update

--- courses/foundation/intro-to-javascript/week2/event.js
+++ courses/foundation/intro-to-javascript/week2/event.js
@@ -1,4 +1,4 @@
-function getEventWeekday(daysToAdd = 5) {
+function getEventWeekday(daysToAdd) {
   const weekDays = [
     "Sunday",
     "Monday",
@@ -10,16 +10,15 @@ function getEventWeekday(daysToAdd = 5) {
   ];
 
   const date = new Date();
-  const WeekDay = date.getDay(); // 0-6 (0 = Sunday)
-
-  // Use remainder operator (%) to wrap around the array
+  const WeekDay = date.getDay();
   const EventDay = (WeekDay + daysToAdd) % weekDays.length;
 
   return weekDays[EventDay];
 }
 
-console.log(getEventWeekday(6));
-console.log(getEventWeekday(3));
+console.log(getEventWeekday(7));
+console.log(getEventWeekday(1));
 
+//function getEventWeekday with daysToAdd parameter,
 // declared variable in array weekDays Sunday - Saturday,
 // use new date() to access current date and getDay() to get current weekday 0-6,
