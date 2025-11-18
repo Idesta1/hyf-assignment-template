313bd30ca30ecb4c17c5dafff87167f161928bdc
Iglesia Desta <iglesiad25@gmail.com>
2025-11-18 19:22:19 +0100

    Create note.js

---
+++ courses/foundation/intro-to-javascript/week3/note.js
@@ -0,0 +1,31 @@
+const notes = [];
+function saveNote(content, id) {
+  notes.push({ content: content, id: id });
+}
+
+saveNote("Pick up groceries", 1);
+saveNote("Do laundry", 2);
+
+console.log(notes);
+
+// get a note
+
+function getNote(id) {
+  return notes.find((note) => note.id === id);
+}
+
+const firstNote = getNote(1);
+const secondNote = getNote(2);
+console.log(firstNote);
+console.log(secondNote);
+
+// log out notes
+function logOutNotesFormatted() {
+  notes.forEach((note) => {
+    console.log(
+      `The note with id: ${note.id}, has the following note text: ${note.content}`
+    );
+  });
+}
+
+logOutNotesFormatted();
