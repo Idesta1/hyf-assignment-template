50ee277a13a7aa3417c5879e9fb71f1b00523362
Iglesia Desta <iglesiad25@gmail.com>
2025-12-10 21:27:08 +0100

    Update db.sql
    Adjusted all changes requested

--- courses/foundation/databases/db.sql
+++ courses/foundation/databases/db.sql
@@ -8,7 +8,7 @@ SELECT COUNT(*) FROM task;
  SELECT * FROM task WHERE status_id = 3;
 
 --4 Find all the tasks that are not marked as done.
-SELECT FROM task WHERE status_id IS NOT 3;
+SELECT * FROM task WHERE status_id != 3;
 
 --5 Get all the tasks, sorted with the most recently created first.
 SELECT * FROM task ORDER BY created DESC;
@@ -17,20 +17,14 @@ SELECT * FROM task ORDER BY created DESC;
 SELECT * FROM task ORDER BY created DESC LIMIT 1;
 
 --7 Get the title and due date of all tasks where the title or description contains database.
-SELECT title, due_date FROM task WHERE title LIKE '%database%' AND description LIKE '%database%';
+SELECT title, due_date FROM task WHERE title LIKE '%database%' OR description LIKE '%database%';
 
 --8 Get the title and status (as text) of all tasks.
-SELECT title, CASE status_id 
-WHEN 1 THEN 'Not Started' 
-WHEN 2 THEN 'In progress' 
-WHEN 3 THEN 'Done' 
-ELSE 'Unknown' 
-END AS 
-status_text FROM task; 
+SELECT t.title, s.name FROM task t LEFT JOIN status s ON  t.status_id = s.id;
 
 --9 Get the name of each status, along with a count of how many tasks have that status.
-SELECT status_id AS status_name , COUNT(*) AS status_count FROM task GROUP BY status_id;
+SELECT s.name AS status, COUNT(t.id) AS status_count FROM status s LEFT JOIN task t ON t.status_id = s.id GROUP BY s.id, s.name;
 
 --10 Get the names of all statuses, sorted by the status with most tasks first.
 SELECT s.name, COUNT(*) AS status_count FROM task t JOIN status s ON t.status_id = s.id 
-GROUP BY s.name;
+GROUP BY s.name ORDER BY status_count DESC;
