e0b41664b6752a6ca4076a02b07dbce99d5cc227
Iglesia Desta <iglesiad25@gmail.com>
2025-12-19 22:07:54 +0100

    Update exercise1.js
    update

--- courses/foundation/intro-to-backend/exercise1.js
+++ courses/foundation/intro-to-backend/exercise1.js
@@ -8,9 +8,15 @@ app.get("/", (req, res) => {
 });
 
 app.get("/currentYear", (req, res) => {
-  const currentDate = new Date();
-  let currentYear = currentDate.getFullYear();
-  res.send(`It is ${currentYear}`);
+  // 1- argument processing can be done here if needed(read data, check if it is valid)
+
+  // 2- action / logic can be done here
+  const currentYear = new Date().getFullYear();
+
+  // 3- response formation
+  res.json({
+    year: currentYear,
+  });
 });
 
 app.listen(port, () => {
