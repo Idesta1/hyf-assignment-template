41c4ed2d7293a7baf82ef5c46f0d27f07f76bb7e
Iglesia Desta <iglesiad25@gmail.com>
2026-01-06 11:15:06 +0100

    update hyfBay
    added renderProducts function

--- courses/foundation/intro-to-frontend/HYFBay/main.js
+++ courses/foundation/intro-to-frontend/HYFBay/main.js
@@ -4,8 +4,21 @@ const products = getAvailableProducts();
 console.log(products);
 
 // This should create the ul and the li's with the individual products details
+
 function renderProducts(products) {
-  // your code goes here
+  const ul = document.querySelector("#product-list");
+
+  products.forEach((product) => {
+    const li = document.createElement("li");
+    li.innerText = `Item: ${product.name} - Price: $${product.price.toFixed(
+      2
+    )} - Rating: ${product.rating}/10`;
+    li.style.marginLeft = "30px";
+    li.style.marginBottom = "10px";
+    li.style.listStyleType = "disc";
+    li.style.marginTop = "20px";
+    ul.appendChild(li);
+  });
 }
 
-renderProducts(products); 
+renderProducts(products);
