13f57853c3725fdbd661838f4338fcd4d235b846
Iglesia Desta <iglesiad25@gmail.com>
2026-01-06 10:41:21 +0100

    Update hogwarts.js

--- courses/foundation/intro-to-frontend/HogwartsHouseGenerator/hogwarts.js
+++ courses/foundation/intro-to-frontend/HogwartsHouseGenerator/hogwarts.js
@@ -1,30 +1,33 @@
-
-
 // Hogwarts house generator
 // create an h1,label,input,button and p element using createElement.
 // used append instead of appendChild to make my code compact and add multiple items at once.
 // added style
 const h1Element = document.createElement("h1");
-h1Element.textContent = "Hogwarts House generator";
 const labelElement = document.createElement("label");
-labelElement.textContent = "Username:";
 const inputElement = document.createElement("input");
+const button = document.createElement("button");
+const para = document.createElement("p");
+
+h1Element.textContent = "Hogwarts House generator";
+labelElement.textContent = "Username:";
+
 inputElement.id = "name-input";
 inputElement.type = "text";
 inputElement.placeholder = "Enter your name..";
-const button = document.createElement("button");
+
 button.textContent = "Submit";
 button.style.marginLeft = "10px";
 button.style.backgroundColor = "lightgreen";
 button.id = "my-button";
-const para = document.createElement("p");
+
 para.id = "myP";
+
 document.body.append(h1Element, labelElement, inputElement, button, para);
 
 //assigning variables and functions
 
 let username;
-let houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+const houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
 
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -33,12 +36,10 @@ function shuffle(array) {
   }
 }
 
-document.getElementById("my-button").onclick = function () {
+button.onclick = function () {
   username = document.getElementById("name-input").value;
+  shuffle(houses);
 
-
-shuffle(houses);
-
-let message = `${username} belongs in House of, ${houses[0]}!`;
-document.getElementById("myP").textContent = message;
+  let message = `${username} belongs in House of, ${houses[0]}!`;
+  document.getElementById("myP").textContent = message;
 };
