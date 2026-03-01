b267c3257a290b1ecec0009f17cf6d2d71ff6e9e
Iglesia Desta <iglesiad25@gmail.com>
2026-03-01 15:31:36 +0100

    modify excercise 8 on using AI session in class

--- assets/courses/frontend/using-ai-in-development/session-materials/exercise8.js
+++ assets/courses/frontend/using-ai-in-development/session-materials/exercise8.js
@@ -5,11 +5,24 @@ function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
+function titleCase(sentence) {
+  if (!sentence) return "";
+  return sentence
+    .split(" ")
+    .map((word) => capitalize(word))
+    .join(" ");
+}
+
 function main() {
   console.log("--- capitalize ---");
   console.log(capitalize("hello"));
   console.log(capitalize("WORLD"));
   console.log(capitalize("javaScript"));
+
+  console.log("--- titleCase ---");
+  console.log(titleCase("hello world")); // Hello World
+  console.log(titleCase("javaScript is fun")); // Javascript Is Fun
+  console.log(titleCase("MAKE THIS TITLE CASE")); // Make This Title Case
 }
 
 main();
