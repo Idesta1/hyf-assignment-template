86b97a965e883858746f2e87fb9a565eff9c489f
Iglesia Desta <iglesiad25@gmail.com>
2026-03-24 22:07:13 +0100

    update
    added structure and style and some of the currency converter logic and API integration.

--- assets/courses/frontend/advanced-javascript/week3/script.js
+++ assets/courses/frontend/advanced-javascript/week3/script.js
@@ -1,3 +1,20 @@
+const convertedAmountElement = document.getElementById("convertedAmount");
+convertedAmountElement.addEventListener("click", convertCurrency);
+result.textContent = `${convertedAmount}`;
+
+async function convertCurrency() {
+  try {
+    const response = await fetch("https://open.er-api.com/v6/latest/USD");
+    const data = await response.json();
+    console.log(data);
+    const rate = data.rates.EUR;
+    const convertedAmount = amount * rate;
+    console.log();
+  } catch (error) {
+    console.error("Error fetching exchange rates:", error);
+  }
+}
+convertCurrency();
 const fromCurrency = document.getElementById("fromCurrency");
 const toCurrency = document.getElementById("toCurrency");
 const amountInput = document.getElementById("amount");
